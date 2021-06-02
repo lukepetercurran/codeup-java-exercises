821033af84dc6b18bbe859ad349673ed8110beb6
Luke Curran <luke.peter.curran54@gmail.com>
2021-06-02 12:25:29 -0500

    finished OOP walkthrough

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,5 +4,14 @@ public class InputTest {
     public static void main(String[] args) {
         Input in = new Input();
         System.out.println(in.getString());
+
+        System.out.println(in.getInt());
+
+        System.out.println(in.getInt(0, 100));
+
+        System.out.println(in.yesNo());
+
+        System.out.println(in.getDouble());
+        System.out.println(in.getDouble(1, 2));
     }
 }
