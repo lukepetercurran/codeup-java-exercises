106d978e65f08a73f5cc02969350279f69dd65c7
Luke Curran <luke.peter.curran54@gmail.com>
2021-06-03 13:36:10 -0500

    exercise confusion

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,7 +1,27 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(numbers); //this returns the id. We dont want the id, we want the literal array
-        System.out.println(numbers.toString());
+        System.out.println(Arrays.toString(numbers));
     }
+
+  class Person {
+        String name = "Mary";
+  }
+
+  Person p1 = new Person();
+  Person p2 = new Person();
+  Person p3 = new Person();
+
+  Person[] people = {p1, p2, p3};
+
+  for (Person p : people) {
+      p
+    }
+
+
+
+
 }
