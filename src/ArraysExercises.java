7910df6f8f0783164267711f684f1b5afa55ee5b
Luke Curran <luke.peter.curran54@gmail.com>
2021-06-03 10:51:20 -0500

    java arrays lec

---
+++ src/ArraysExercises.java
@@ -0,0 +1,7 @@
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers); //this returns the id. We dont want the id, we want the literal array
+        System.out.println(numbers.toString());
+    }
+}
