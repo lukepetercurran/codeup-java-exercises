2d3c0771e35e0cfa5acfd70f910228eca1eab3ba
Luke Curran <luke.peter.curran54@gmail.com>
2021-06-07 17:02:27 -0500

    first exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -10,8 +10,26 @@ public class Student {
 
     public Student(String name){
         this.name = name;
-
+        this.grades = new ArrayList<>();
     }
 
+    // returns the student's name
+    public String getName(){
+        return name;
+    }
+
+    // adds grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        return (double) grades.stream().reduce(Integer::sum).get() / grades.size();
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
