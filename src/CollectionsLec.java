ffe426f557141eb3f731fee519d2c3af0878c54f
Luke Curran <luke.peter.curran54@gmail.com>
2021-06-07 12:31:16 -0500

    lunch

---
+++ src/CollectionsLec.java
@@ -0,0 +1,99 @@
+import java.lang.reflect.Array;
+import shapes.Square;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CollectionsLec {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4};
+        ArrayList <Integer> collectionOfNums = new ArrayList<>();
+        collectionOfNums.add(16);
+        collectionOfNums.add(44);
+        collectionOfNums.add(32);
+        collectionOfNums.add(1, 24);
+
+        for (int i = 0; i < collectionOfNums.size(); i++){
+            System.out.println(collectionOfNums.get(i));
+        }
+
+        for(Integer collectionOfNum : collectionOfNums){
+            System.out.println(collectionOfNums);
+        }
+
+        System.out.println(numbers);
+        System.out.println(collectionOfNums);
+
+        System.out.println(collectionOfNums.contains(44));
+        System.out.println(collectionOfNums.indexOf(44));
+        System.out.println(collectionOfNums.lastIndexOf(44));
+        ArrayList<String> studentNames = new ArrayList<>();
+        if (studentNames.isEmpty()) {
+            System.out.println("Let's fill in the list of Student names.");
+        }
+
+         boolean resultOfRemove = studentNames.remove("Beatrice");
+
+        System.out.println(collectionOfNums);
+        collectionOfNums.remove((Integer) 44);
+        System.out.println(collectionOfNums);
+
+        ArrayList<Double> collectionOfDoubles = new ArrayList<>();
+        collectionOfDoubles.add(3.14);
+        collectionOfDoubles.add(56.0);
+        collectionOfDoubles.add(7.77777777);
+        collectionOfDoubles.add((double) 2);
+
+        System.out.println(collectionOfDoubles);
+        collectionOfDoubles.remove( 2);
+        System.out.println(collectionOfDoubles);
+
+
+        ArrayList stuff = new ArrayList();
+        stuff.add("Beatrice");
+        stuff.add(13);
+        stuff.add(new Square(5));
+
+        for (Object thing : stuff) {
+            System.out.println(thing);
+        }
+
+        System.out.println(stuff.get(2));
+//        System.out.println(); stuff.get(2).getArea();
+
+        System.out.println(collectionOfNums);
+        collectionOfNums.set(2,88);
+        System.out.println(collectionOfNums);
+
+        System.out.println("/////////");
+
+        HashMap<Character, Square> squares = new HashMap<>();
+        squares.put('A', new Square(5));
+        squares.put('B', new Square(10));
+        System.out.println(squares.get('A').getArea());
+//        System.out.println(squares.get('C').getArea());
+        System.out.println(
+                squares
+                .getOrDefault('B', new Square(0))
+                .getArea()
+        );
+        System.out.println(squares.containsKey('B'));
+        System.out.println(squares.containsValue(new Square(5)));
+        System.out.println(squares.containsValue(sq));
+        System.out.println(sq);
+        System.out.println(squares.get('Q'));
+        squares.remove('Q');
+        System.out.println(squares.get('Q'));
+        squares.remove('C', sq);
+        System.out.println(squares.containsValue(sq));
+        squares.replace('D', new Square(4));
+        System.out.println(sq.getArea());
+        System.out.println(squares.get('D').getArea());
+        squares.replace('E', new Square(15));
+//        System.out.println(squares.get('E').getArea());
+        squares.clear();
+        System.out.println(squares.size());
+        System.out.println(squares.isEmpty());
+
+    }
+}
---
+++ src/grades/Student.java
@@ -0,0 +1,17 @@
+package grades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private List<Integer> grades;
+
+
+    public Student(String name){
+        this.name = name;
+
+    }
+
+
+}
